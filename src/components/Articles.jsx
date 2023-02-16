20713350f2be91ac7b8e806bfcaa3296b9822233
wbsantiago <will.jimmy27@hotmail.com>
2023-02-16 19:27:08 -0300

    about us and header done

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,22 +1,40 @@
 import "./articles.css"
-import imageOffice from '../assets/office-work.jpg'
+import esbocoProject from '../assets/espoço-projeto.png'
+import ceoPhotos from '../assets/ceoPhotos.png'
+
 
 function Articles() {
     return (
-        <div className="articles">
+        <div id="aboutUs" className="articles">
             <div className="first-article">
                 <img 
                     className="image-work" 
-                    src={imageOffice} 
+                    src={ceoPhotos} 
                     alt='imagem de reunião no escritório'
                 >
                 </img>
                 <div 
                     className='article-text-body'
                 >
-                    <h2 className="title-first-article">Projetos Elaborados especialmente para você!</h2>
-                    <p className="text-first-article">Quando se trata de construir, reformar ou decorar uma casa, muitas vezes os proprietários têm uma visão clara do que querem, mas não sabem como transformar essa visão em realidade. É aí que entra a WD Arquitetura e Engenharia. Com sua expertise e conhecimento técnico, nós podemos ajudar a transformar seus sonhos em uma casa que atenda todas as suas necessidades e desejos.</p>
+                    <h2 className="title-first-article">Quem Somos?</h2>
+                    <p className="text-first-article">Somos um escritório de arquitetura e engenharia localizado na cidade de Arapongas e região, com foco em projetos residenciais, comerciais, execução de obra e laudos técnicos. Fundado por <span className="bold-text">Willian B. Santiago</span>, graduado em Engenharia Civil e Pós-graduando em Engenharia de Avaliação e Perícias, e <span className="bold-text">Deivid R. Ribeiro</span>, graduado em Arquitetura e Urbanismo, somos uma equipe altamente qualificada e comprometida em oferecer soluções eficientes e personalizadas para nossos clientes.</p>
                 </div>   
+            </div>
+        
+        
+            <div className="second-article">
+                <div 
+                    className='article-text-body'
+                >
+                    <h2 className="title-second-article">Projetos Elaborados especialmente para você!</h2>
+                    <p className="text-second-article">Quando se trata de construir, reformar ou decorar uma casa, muitas vezes os proprietários têm uma visão clara do que querem, mas não sabem como transformar essa visão em realidade. É aí que entra a WD Arquitetura e Engenharia. Com sua expertise e conhecimento técnico, nós podemos ajudar a transformar seus sonhos em uma casa que atenda todas as suas necessidades e desejos.</p>
+                </div>   
+                <img 
+                    className="image-work2" 
+                    src={esbocoProject} 
+                    alt='imagem de reunião no escritório'
+                >
+                </img>
             </div>
         </div>
     )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import wppLogo from "../assets/wpp-icon.svg"
 
 function Header() {
     return (
-        <div className='header'>
+        <div id='home' className='header'>
             <div>
                 <img src={logowd} className="header-logo" alt="logo-website" />
             </div>
@@ -12,7 +12,7 @@ function Header() {
             <nav className='header-nav' id="navbar">
                 <ul className='header-nav-ul'>
                 <li><a href="#home">Home</a></li>
-                <li><a href="#projects">Sobre Nós</a></li>
+                <li><a href="#aboutUs">Sobre Nós</a></li>
                 <li><a href="#projects">Projetos</a></li>
                 <li><a href="#contact">Contato</a></li>
                 <li><a href="https://api.whatsapp.com/send/?phone=5543999876724&text&type=phone_number&app_absent=0" target="_blank"><img width="30px" src={wppLogo} alt="" /></a></li>
